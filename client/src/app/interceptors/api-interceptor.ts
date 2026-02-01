1149a92221b34fe20140e051c4aff4182908753c
Rachel Shulzinger <r0548462219@gmail.com>
2026-02-01 19:50:18 +0200

    Fix API URL: add /api suffix

--- client/src/app/interceptors/api-interceptor.ts
+++ client/src/app/interceptors/api-interceptor.ts
@@ -1,7 +1,7 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const apiInterceptor: HttpInterceptorFn = (req, next) => {
-  const baseUrl = 'https://clickup-angular-final-project-server-hecm.onrender.com';
+  const baseUrl = 'https://clickup-angular-final-project-server-hecm.onrender.com/api';
   const apiReq = req.clone({
     url: `${baseUrl}${req.url.startsWith('/') ? '' : '/'}${req.url}`
   });
